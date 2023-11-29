68634cc9e7d352ba7f1ea85faa116769ef65123f
yainage90 <yainage90@gmail.com>
2023-11-29 16:46:06 +0900

    settings env파일 명시 제거

--- app/config.py
+++ app/config.py
@@ -4,23 +4,11 @@ from pydantic import Field
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
-
     APP_ENV: str
 
     ELASTICSEARCH_HOST: str
     ELASTICSEARCH_USER: str | None = Field(default=None)
     ELASTICSEARCH_PASSWORD: str | None = Field(default=None)
-
-
-"""
-class DevelopmentSettings(Settings):
-    model_config = SettingsConfigDict(env_file=".env.development")
-
-
-class ProductionSettings(Settings):
-    model_config = SettingsConfigDict(env_file=".env")
-"""
 
 
 @lru_cache
