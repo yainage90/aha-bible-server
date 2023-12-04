a71d8f877a282b5c17fdf8ee61a41ad85be05577
yainage90 <yainage90@gmail.com>
2023-12-04 23:35:14 +0900

    docker run background

--- run_local.sh
+++ run_local.sh
@@ -1,1 +1,7 @@
-docker run --name aha-bible-server --rm -p 8000:8000 --network host --env-file ./.env aha-bible-server
+docker run\
+  --name aha-bible-server\
+  --rm -d\
+  -p 8000:8000\
+  --network host\
+  --env-file ./.env\
+  aha-bible-server
